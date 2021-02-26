c5047ebae16cc9fed279d95fcfef62b51d6a735c
danaebantana <danae.bantana@hotmail.com>
2021-02-26 12:22:53 +0200

    Contact class updated!

--- app/src/main/java/com/unipi/danaeb/myagenda/Contact.java
+++ app/src/main/java/com/unipi/danaeb/myagenda/Contact.java
@@ -1,10 +1,6 @@
 package com.unipi.danaeb.myagenda;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-import java.io.Serializable;
-
-public class Contact implements Parcelable {
+public class Contact{
 
     private String name;
     private String phoneNumber;
@@ -16,51 +12,16 @@ public class Contact implements Parcelable {
         setSelected(false);
     }
 
-    protected Contact(Parcel in) {
-        name = in.readString();
-        phoneNumber = in.readString();
-    }
+    public String getName() { return name; }
 
-    public static final Creator<Contact> CREATOR = new Creator<Contact>() {
-        @Override
-        public Contact createFromParcel(Parcel in) {
-            return new Contact(in);
-        }
+    public void setName(String name) { this.name = name; }
 
-        @Override
-        public Contact[] newArray(int size) {
-            return new Contact[size];
-        }
-    };
+    public String getPhoneNumber() { return phoneNumber; }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
 
     public boolean isSelected() { return selected; }
 
     public void setSelected(boolean selected) { this.selected = selected; }
 
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(name);
-        dest.writeString(phoneNumber);
-    }
 }
